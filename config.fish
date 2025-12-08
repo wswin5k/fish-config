4cdef374ba00e1bd303da709592c2ea684e47d50
wswin <wswin@protonmail.com>
2025-12-08 20:30:36 +0100

    Haskell vars

--- config.fish
+++ config.fish
@@ -8,3 +8,5 @@ function fish_title
     set -q argv[1]; or set argv (fish_prompt_pwd_dir_length=0 prompt_pwd);
     echo $argv
 end
+
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH /home/fed/.ghcup/bin # ghcup-env
