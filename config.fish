cd417140a97e44b8a22e499781b0c5e5cc53cf35
wswin <wswin@protonmail.com>
2025-12-08 20:36:00 +0100

    Change tab title function

--- config.fish
+++ config.fish
@@ -5,8 +5,10 @@ end
 set -g fish_prompt_pwd_dir_length 0
 
 function fish_title
-    set -q argv[1]; or set argv (fish_prompt_pwd_dir_length=0 prompt_pwd);
-    echo $argv
+    set -q argv[1]; or set argv fish
+    # Looks like ~/docs/fish: git log
+    # or /etc/apt: fish
+    echo (fish_prompt_pwd_dir_length=0 prompt_pwd): $argv;
 end
 
 set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH /home/fed/.ghcup/bin # ghcup-env
